0b499bc7bf8d2382fe702a219b8fc0b8cabb3002
Nate Festinger <nathaniel.festinger@gmail.com>
2021-04-03 16:35:56 -0700

    Clean up LoginForm

---
+++ app/javascript/components/LoginForm.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { Button, Divider, TextField, Typography, withStyles } from '@material-ui/core';
+
+import { connect } from 'react-redux';
+import { logIn } from '../actions/session-actions';
+
+const styles = {
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    '& > button': {
+      marginTop: '10px'
+    },
+    '& > *': {
+      display: 'block'
+    }
+  }
+};
+
+class LoginForm extends React.Component {
+  handleSubmit = () => {
+    if (!this.validateInputs()) return;
+
+    const { username, password } = this.state.formData;
+    this.props.logIn({ username, password });
+  }
+
+  validateInputs = () => {
+    const { username, password } = this.state.formData;
+    const errors = [];
+    if (!username) errors.push("Username must not be blank.");
+    if (!password) errors.push("Password must not be blank.");
+
+    if (errors.length) {
+      this.setState({ errors });
+      return false;
+    }
+
+    return true;
+  }
+
+  handleInputChange = (inputName, e) => {
+    this.setState(
+      { formData: { ...this.state.formData, [inputName]: e.currentTarget.value } }
+    );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {
+        username: '',
+        password: '',
+      },
+      errors: []
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors.length) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  render() {
+    const errMsgs = this.state.errors.map((err, idx) => (<Typography key={idx} color="error">{err}</Typography>))
+    const { formData } = this.state;
+    return (
+      <React.Fragment>
+        {errMsgs}
+        <form className={this.props.classes.formContainer}>
+          <TextField 
+            id="username" 
+            type="text"
+            label="Username" 
+            margin="dense"
+            required
+            value={formData.username}
+            onChange={e => this.handleInputChange('username', e)}
+          />
+          <TextField
+            id="password"
+            type="password"
+            label="Password"
+            margin="dense"
+            required
+            value={formData.password}
+            onChange={e => this.handleInputChange('password', e)}
+          />
+          <Button 
+            color="primary" 
+            variant="outlined" 
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </Button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  errors: state.session.errors
+});
+
+const mapDispatchToProps = dispatch => ({
+  logIn: (user) => dispatch(logIn(user))
+});
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(withStyles(styles)(LoginForm));
