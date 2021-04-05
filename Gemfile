665ac6983d5d14998e39a2e9adbbbffc06377af6
Nate Festinger <nathaniel.festinger@gmail.com>
2021-04-04 18:02:43 -0700

    Create DB seeds

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-rails'
+  gem "faker", "~> 2.17"
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+USER_COUNT = 10
+USER_COUNT.times do 
+  User.create(
+    username: Faker::Name.unique.name,
+    email: Faker::Internet.email,
+    password: '123456'  
+  )
+end
+
+80.times do 
+  Vision.create(
+    name: Faker::Lorem.paragraph(sentence_count: 1, random_sentences_to_add: 1),
+    nickname: Faker::Lorem.words(number: rand(2..6)).map(&:capitalize).join(' '),
+    description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+    motivation: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+    impact: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+    user_id: rand(0...USER_COUNT)
+  )
+end
