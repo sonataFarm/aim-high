9bea73c4ee7f296a5217423ea1124a9de5c0759b
Nate Festinger <nathaniel.festinger@gmail.com>
2021-04-28 17:44:23 -0700

    Add a ThemeProvider

--- app/javascript/components/Root.js
+++ app/javascript/components/Root.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-
+import { ThemeProvider } from '@material-ui/styles';
 import App from './App';
+import theme from '../styles/theme';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </Provider>
 );
 
