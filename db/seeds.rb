2065b6cbf73132ffe9e20cef16c723e26a0fc522
Nate Festinger <nathaniel.festinger@gmail.com>
2021-05-13 15:30:30 -0700

    Clean up seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,44 @@
 require 'faker'
 
-USER_COUNT = 10
-USER_COUNT.times do
-  User.create(
+10.times do
+  u = User.create(
     username: Faker::Internet.username,
     email: Faker::Internet.email,
     password: '123456'  
   )
-end
 
-VISION_COUNT = USER_COUNT * 8
-VISION_COUNT.times do
-  Vision.create(
-    user: User.all.sample,
-    title: Faker::Lorem.words(number: rand(2..5)).map(&:capitalize).join(' '),
-    description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    motivation: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    impact: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-  )
-end
+  rand(3..8).times do
+    v = Vision.create(
+      user: u,
+      title: Faker::Lorem.words(number: rand(2..5)).map(&:capitalize).join(' '),
+      description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+      motivation: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+      impact: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+    )
 
-GOAL_COUNT = VISION_COUNT * 5
-GOAL_COUNT.times do
-  user = nil
+    rand(0..5).times do 
+      g = Goal.create(
+        user: u,
+        vision: v,
+        title: Faker::Lorem.words(number: rand(2..5)).map(&:capitalize).join(' '),
+        description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+        motivation: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+        impact: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+        strategy: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+        deadline: Faker::Date.between(from: '2021-04-04', to: '2021-12-31'),
+        evidence: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+        satisfaction: Faker::Lorem.paragraph_by_chars(number: rand(180...300))
+      )
 
-  while true
-    user = User.all.sample
-    break unless user.visions.empty?
+      rand(0..5).times do
+        Obstacle.create(
+          goal: g,
+          description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
+          solution: Faker::Lorem.paragraph_by_chars(number: rand(180...300))
+        )
+      end
+    end
   end
-
-  Goal.create(
-    user_id: user.id,
-    vision: user.visions[rand(0...user.visions.count)],
-    title: Faker::Lorem.words(number: rand(2..5)).map(&:capitalize).join(' '),
-    description: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    motivation: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    impact: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    strategy: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    deadline: Faker::Date.between(from: '2021-04-04', to: '2021-12-31'),
-    evidence: Faker::Lorem.paragraph_by_chars(number: rand(180...300)),
-    satisfaction: Faker::Lorem.paragraph_by_chars(number: rand(180...300))
-  )
 end
 
 User.first.update(username: 'sonataFarm')
