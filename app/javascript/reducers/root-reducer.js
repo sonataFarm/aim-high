977565f1b589d9316a68d2d0a1e0c6e1d8579428
Nate Festinger <nathaniel.festinger@gmail.com>
2021-05-16 16:17:11 -0700

    Clear state on logout

--- app/javascript/reducers/root-reducer.js
+++ app/javascript/reducers/root-reducer.js
@@ -2,9 +2,20 @@ import { combineReducers } from 'redux';
 import entities from './entities/entities-reducer';
 import session from './session-reducer';
 import ui from './ui/ui-reducer';
+import { REMOVE_CURRENT_USER } from '../actions/session-actions';
 
-export default combineReducers({
+const appReducer = combineReducers({
   entities,
   session,
   ui
 });
+
+const rootReducer = (state, action ) => {
+  if (action.type === REMOVE_CURRENT_USER) {
+    return appReducer(undefined, action);
+  }
+    
+  return appReducer(state, action);
+}
+
+export default rootReducer;
