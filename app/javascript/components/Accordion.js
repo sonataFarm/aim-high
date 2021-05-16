00d319a0350198cc7d1b9f7363b34673b54e4e03
Nate Festinger <nathaniel.festinger@gmail.com>
2021-05-16 12:47:50 -0700

    Tweak styles

--- app/javascript/components/Accordion.js
+++ app/javascript/components/Accordion.js
@@ -3,17 +3,24 @@ import { Accordion as MuiAccordion, AccordionSummary, AccordionDetails, Typograp
 import { ExpandMore } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   accordion: {
     width: '100%'
+  },
+  accordionSummary: {
+    backgroundColor: theme.palette.primary.main,
+    color: 'white',
+    '& svg': {
+      color: 'white'
+    }
   }
-});
+}));
 
 const Accordion = ({ title, children }) => {
   const classes = useStyles();
   return (
     <MuiAccordion id="hi" classes={{ root: classes.accordion }}>
-      <AccordionSummary expandIcon={<ExpandMore />}>
+      <AccordionSummary expandIcon={<ExpandMore />} classes={{ root: classes.accordionSummary }}>
         <Typography variant="body1">{ title }</Typography>
       </AccordionSummary>
       <AccordionDetails>
